9e41d3c78a8fe6d088b6e6a20b3b396245179d95
AnastasiiaAleks <chiffka@gmail.com>
2023-09-18 21:37:01 +0300

    Задачи 0, й, 5, 7

---
+++ Seminar1/Task7/Program.cs
@@ -0,0 +1,18 @@
+﻿//  Напишите программу, которая принимает на 
+//  вход трёхзначное число и 
+//  на выходе показывает последнюю цифру этого числа.
+// 	456 -> 6
+// 	782 -> 2
+// 	918 -> 8
+
+Console.WriteLine("enter a number");
+int number = int.Parse(Console.ReadLine());
+
+if (number >= 100 && number<1000)
+{
+    Console.WriteLine("resilt is " + number % 10);
+}
+else
+{
+    Console.WriteLine("Введите трехзначное число");
+}
