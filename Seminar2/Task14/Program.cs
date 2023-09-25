a6a40d2e997d7f4c19493c6d7eb0136dec4541dc
AnastasiiaAleks <chiffka@gmail.com>
2023-09-25 22:09:49 +0300

    задача 14 - кратность числа двум одновременно числам

---
+++ Seminar2/Task14/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход
+// число и проверяет, кратно ли оно одновременно
+// 7 и 23.
+
+// 14 -> нет 
+// 46 -> нет 
+// 161 -> да
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number%7==0&&number%23==0)
+{
+    Console.WriteLine($"Число {number} кратно 7 и 23");
+}
+else
+{
+    Console.WriteLine($"Число {number} не кратно 7 и 23");
+}
