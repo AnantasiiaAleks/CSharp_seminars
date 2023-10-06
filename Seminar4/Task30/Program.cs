2fb95ea9f95f8b780b67e6d143d61fb98136a8b6
AnastasiiaAleks <chiffka@gmail.com>
2023-10-07 00:25:59 +0300

    одифицировали задачу 30 - через метод

--- Seminar4/Task30/Program.cs
+++ Seminar4/Task30/Program.cs
@@ -3,10 +3,27 @@
 // в случайном порядке.
 // [1,0,1,1,0,1,0,0]
 
-int[] array = new int[8];
-array[0] = 0;
-for(int i = 0; i < array.Length; i++)
+// int[] array = new int[8];
+// array[0] = 0;
+// for(int i = 0; i < array.Length; i++)
+// {
+//     array[i] = new Random().Next(0, 2);
+//     Console.Write(array[i] + " ");
+// } 
+
+int[] FillArray(int size)
 {
-    array[i] = new Random().Next(0, 2);
-    Console.Write(array[i] + " ");
-} 
+    int[] array = new int[size];
+
+    for (int index = 0; index < size; index++)
+    {
+        array[index] = new Random().Next(0, 2);
+        Console.Write(array[index] + " ");
+    }
+    return array;
+}
+
+Console.Write("Введите длину массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+FillArray(size);
+
