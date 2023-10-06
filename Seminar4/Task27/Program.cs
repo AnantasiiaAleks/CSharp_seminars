0228b63dab1cae70f82660de5c694cf413725846
AnastasiiaAleks <chiffka@gmail.com>
2023-10-07 01:30:45 +0300

    Задача 27 - сумма цифр в числе

---
+++ Seminar4/Task27/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 27: Напишите программу, которая принимает
+// на вход число и выдаёт сумму цифр в числе.
+
+int ReadNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumDigits(int a)
+{
+    int sum = 0;
+    int i = 0;
+     while (a >= 1 || a <= -1)
+    {
+        sum = sum + (a % 10);
+        a /= 10;
+        i++;
+    }
+    return sum;
+}
+
+int number = ReadNumber("Введите число: ");
+if (number != 0)
+{
+    int result = SumDigits(number);
+    Console.WriteLine($"Сумма цифр числа {number} составляет {Math.Abs(result)}");
+}
+else
+{
+    Console.WriteLine("Введите число, отличное от нуля");
+}
+
