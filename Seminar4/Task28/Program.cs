c833967897c7c3f5a0103bf13f6a61eb6c3915f5
AnastasiiaAleks <chiffka@gmail.com>
2023-10-06 22:24:58 +0300

    задача 28 - факториал числа N

---
+++ Seminar4/Task28/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход
+// число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+int GetNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Factorial(int n)
+{
+    int fact = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        fact *= i;
+    }
+    return fact;
+}
+
+int number = GetNumber("Введите целое положительное число: ");
+if (number >= 1)
+{
+    int result = Factorial(number);
+    Console.WriteLine($"произведение чисел от 1 до {number} = {result}");
+}
