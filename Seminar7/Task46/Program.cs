77c8599c7d3fea1c7d1e20a6b7073076d55ad035
AnastasiiaAleks <chiffka@gmail.com>
2023-10-12 00:11:12 +0300

    Задача 46 - создание двухмерного массива

---
+++ Seminar7/Task46/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n,
+// заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+
+int[,] GetRandomMatrix(int rows, int columns, int leftBorder, int rightBorder)
+{
+    int[,] matrix = new int[rows, columns];
+
+    for(int i = 0; i > matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j > matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Random.Shared.Next(leftBorder, rightBorder + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matr)
+{
+    for(int i = 0; i < matr.GetLength(0); i++)
+    {
+        for(int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int rows = 3;
+int columns = 4;
+int leftBorder = -50;
+int rightBorder = 100;
+
+int[,] myMatrix = GetRandomMatrix(rows, columns, leftBorder, rightBorder);
+PrintMatrix(myMatrix);
