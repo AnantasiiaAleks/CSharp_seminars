5e4cb96afab15f742a8c56a3b75ac70c3bdfc911
AnastasiiaAleks <chiffka@gmail.com>
2023-10-12 01:39:45 +0300

    Задача 51 - найти сумму элементов главной линии двухмерного массива

---
+++ Seminar7/Task51/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 51: Задайте двумерный массив.
+// Найдите сумму элементов, 
+// находящихся на главной диагонали
+// (с индексами (0,0); (1;1) и т.д.
+
+int[,] GetRandomMatrix(int rows, int columns, int leftBorder, int rightBorder)
+{
+    int[,] matrix = new int[rows, columns];
+    for(int i =0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Random.Shared.Next(leftBorder, rightBorder + 1);
+        }
+    }
+    return matrix;
+}
+
+int SumElementsOfMainLine(int[,] matrix)
+{
+    int sum = 0;
+    for(int i =0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i == j)
+            {
+                sum += matrix[i, j];
+            }
+        }
+    }
+    return sum;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for(int i =0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+const int Rows = 5;
+const int Columns = 5;
+const int LeftBorder = 1;
+const int RightBorder = 9;
+
+int[,] myMatrix = GetRandomMatrix(Rows, Columns,LeftBorder,RightBorder);
+PrintMatrix(myMatrix);
+int sumMainLine = SumElementsOfMainLine(myMatrix);
+Console.WriteLine($"Сумма элементов главной линии = {sumMainLine}.");
