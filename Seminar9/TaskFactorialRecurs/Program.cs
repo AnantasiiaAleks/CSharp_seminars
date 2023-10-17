687c4fe0867f957dfff2bdf7e736ad1a08189aaf
AnastasiiaAleks <chiffka@gmail.com>
2023-10-18 00:31:43 +0300

    Вычисление факториала числа через рекурсию

---
+++ Seminar9/TaskFactorialRecurs/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача:
+// при помощи рекурсии вывести факториал числа N.
+
+const uint N = 4;   //uint - целое число, которое может быть только положительным
+
+uint Factorial(uint n)
+{
+    if(n == 0 || n == 1)
+    {
+        return 1;
+    }
+    return n * Factorial(n - 1);
+}
+
+uint result = Factorial(N);
+Console.WriteLine(result);
