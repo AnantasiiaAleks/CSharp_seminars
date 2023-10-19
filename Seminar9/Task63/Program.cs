9740519cf32962c61394e922b6a44f482a994b55
AnastasiiaAleks <chiffka@gmail.com>
2023-10-20 00:06:32 +0300

    Задача 63 - вывод последовательноти чисел от 1 до n рекурсией

---
+++ Seminar9/Task63/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все
+// натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1,2,3,4,5"
+
+int GetInt(string message)
+{
+    Console.Write(message);
+    int n = Convert.ToInt32(Console.ReadLine());
+    return n;
+}
+
+void OneToN(int n)
+{
+    if (n == 0)
+    {
+        return;
+    }
+    OneToN(n - 1);
+    Console.WriteLine(n);
+}
+
+int num = GetInt("Дай число: ");
+OneToN(num);
