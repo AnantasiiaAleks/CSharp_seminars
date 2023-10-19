136b991d82afac584115f1e21b27afca19c6b7d4
AnastasiiaAleks <chiffka@gmail.com>
2023-10-20 00:17:47 +0300

    Задача 65 - последовательноть от M до N рекурсия

---
+++ Seminar9/Task65/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все
+// натуральные числа в промежутке от М до N.
+
+
+int GetInt(string message)
+{
+    Console.Write(message);
+    int n = Convert.ToInt32(Console.ReadLine());
+    return n;
+}
+
+void MToN(int m, int n)
+{
+    if (n < m)
+    {
+        return;
+    }
+    MToN(m, n - 1);
+    Console.WriteLine(n);
+}
+
+int firstNum = GetInt("Дай первое число: ");
+int lastNum = GetInt("Дай второе число: ");
+MToN(firstNum, lastNum);
