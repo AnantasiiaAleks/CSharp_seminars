000d71d93ecd67444c97e36aa1d7ae09524c96ad
AnastasiiaAleks <chiffka@gmail.com>
2023-10-21 00:10:07 +0300

    ДЗ - Задача 66. Рекурсия: сумма чисел от M до N

---
+++ Seminar9/Task66_HW/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу,
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int GetInt(string message)
+{
+    Console.Write(message);
+    int n = Convert.ToInt32(Console.ReadLine());
+    return n;
+}
+
+int SumMToN(int m, int n)
+{
+    int sum = m;
+    if (m > n)
+    {
+        return 0;
+    }
+    sum = sum + SumMToN(m + 1, n);
+    return sum;
+}
+
+int firstNum = GetInt("Дай первое число: ");
+int lastNum = GetInt("Дай второе число: ");
+int result = SumMToN(firstNum, lastNum);
+Console.WriteLine(result);
